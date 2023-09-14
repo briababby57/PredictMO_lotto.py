160ef968727073e061b22ae8adef43e55e171677
briababby57 <96610248+briababby57@users.noreply.github.com>
2023-09-14 00:47:59 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,10 +5,11 @@ from collections import OrderedDict
 
 def main():
     file_name = "key:valueDB.tsv"
-    date = input("Pick a date ranging from December of 2022 until September 2023:")
-    pastNumbers = read_in_pastNumbers(key:valueDB.tsv)
-    pastNumbers_arrays = analyze_pastNumbers(pastNumbers)
+    date = input("Pick a date ranging from January 1 of 2022 - Present:")
+    pastNumbers = (Key:Value.tsv)
+    pastNumbers = read_in_pastNumbers(Key:Value.tsv)
+    pastNumbers = analyze_pastNumbers(Key:Value.tsv)
 
     # Retrieve the numbers for the selected date
-    selected_numbers = pastNumbers_arrays[date]
+    selected_numbers = pastNumbers[date]
     print(f"The win
